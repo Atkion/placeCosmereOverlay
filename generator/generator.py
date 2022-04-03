84a0767e6def33866f4403e0b2c95f2f11be212d
David Polák <git@polakdavid.com>
2022-04-03 13:35:16 +0200

    fix: generator x

--- generator/generator.py
+++ generator/generator.py
@@ -54,47 +54,7 @@ def generate_template():
 
     mask = open_mask()
     template = Image.composite(create_blank_image(), canvas, mask)
-    template.save("./template.png")
-
-    # tl = (1710 * 3, 2 * 3)
-    #
-    # final_img = Image.new('RGBA', (6000, 3000))
-    # unmasked_img = Image.new('RGBA', (6000, 3000))
-    # unmasked_img.paste(img, tl)
-    # final_img = Image.composite(final_img, unmasked_img, mask)
-    # final_img.save("generated_template.png")
+    template.save("../template.png")
 
 
 generate_template()
-
-
-# #img = Image.open("stormlight.png")
-#
-# #width = int(img.size[0] / 8)
-# #height = int(img.size[1] / 8)
-#
-# #print(width, " " , height)
-# #print(int(width), " " , int(height))
-# #img = img.resize(( width, height), Image.NEAREST)
-#
-# img = Image.open("rwot.png")
-#
-# width = int(img.size[0])
-# height = int(img.size[1])
-#
-# print(width, " " , height)
-# print(int(width), " " , int(height))
-# img = img.resize(( width, height), Image.NEAREST)
-#
-# img = img.resize(( width * 3 , height * 3), Image.NEAREST)
-#
-# img.save("intermediary.png")
-#
-#
-# tl = (1710 * 3, 2 * 3)
-#
-# final_img = Image.new('RGBA', (6000, 3000))
-# unmasked_img = Image.new('RGBA', (6000, 3000))
-# unmasked_img.paste(img, tl)
-# final_img = Image.composite(final_img, unmasked_img, mask)
-# final_img.save("generated_template.png")
