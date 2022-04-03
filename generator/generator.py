1f6723e5688d20e6cdcd56f96a710978b845208d
David Polák <git@polakdavid.com>
2022-04-03 13:30:58 +0200

    fix: generator x

--- generator/generator.py
+++ generator/generator.py
@@ -1,36 +1,100 @@
 from io import BytesIO
 from PIL import Image, ImageOps
 
-#img = Image.open("stormlight.png")
+from os import listdir
+from os.path import isfile, join, splitext
 
-#width = int(img.size[0] / 8)
-#height = int(img.size[1] / 8)
 
-#print(width, " " , height)
-#print(int(width), " " , int(height))
-#img = img.resize(( width, height), Image.NEAREST)
+PIXELART_FOLDER = "./pixelart/"
 
-img = Image.open("rwot.png")
+FIELD_WIDTH = 6000
+FIELD_HEIGHT = 3000
 
-width = int(img.size[0])
-height = int(img.size[1])
+def parse_coordinates(filename):
+    name = splitext(filename)[0]
+    return name.split("x")
 
-print(width, " " , height)
-print(int(width), " " , int(height))
-img = img.resize(( width, height), Image.NEAREST)
+def normalize_coordinates(coordinates):
+    return (int(coordinates[0]) * 3, int(coordinates[1]) * 3)
 
-img = img.resize(( width * 3 , height * 3), Image.NEAREST)
+def process_image(filename):
+    image = Image.open(f"{PIXELART_FOLDER}/{filename}")
+    width = image.size[0]
+    height = image.size[1]
+    image = image.resize((width * 3, height * 3), Image.Dither.NONE)
+    return image
 
-img.save("intermediary.png")
+def get_pixelart():
+    pixelart = [f for f in listdir(PIXELART_FOLDER) if isfile(join(PIXELART_FOLDER, f))]
+    return pixelart
 
-mask_i = Image.open("mask.png")
-mask = Image.new("1", (6000, 3000), 0)
-mask.paste(mask_i)
+def open_mask():
+    mask_i = Image.open("mask.png")
+    mask = Image.new("1", (FIELD_WIDTH, FIELD_HEIGHT), 0)
+    mask.paste(mask_i)
+    return mask
 
-tl = (1710 * 3, 2 * 3)
+def create_blank_image():
+    return Image.new('RGBA', (FIELD_WIDTH, FIELD_HEIGHT))
 
-final_img = Image.new('RGBA', (6000, 3000))
-unmasked_img = Image.new('RGBA', (6000, 3000))
-unmasked_img.paste(img, tl)
-final_img = Image.composite(final_img, unmasked_img, mask)
-final_img.save("generated_template.png")
+def generate_template():
+    canvas = create_blank_image()
+
+    pixelart = get_pixelart()
+    print(f"Found {pixelart=}")
+
+    for filename in pixelart:
+        print(f"Processing {filename}")
+
+        image = process_image(filename)
+
+        coordinates = parse_coordinates(filename)
+        print(f"{coordinates=}")
+        canvas.paste(image, normalize_coordinates(coordinates))
+
+    mask = open_mask()
+    template = Image.composite(create_blank_image(), canvas, mask)
+    template.save("./template.png")
+
+    # tl = (1710 * 3, 2 * 3)
+    #
+    # final_img = Image.new('RGBA', (6000, 3000))
+    # unmasked_img = Image.new('RGBA', (6000, 3000))
+    # unmasked_img.paste(img, tl)
+    # final_img = Image.composite(final_img, unmasked_img, mask)
+    # final_img.save("generated_template.png")
+
+
+generate_template()
+
+
+# #img = Image.open("stormlight.png")
+#
+# #width = int(img.size[0] / 8)
+# #height = int(img.size[1] / 8)
+#
+# #print(width, " " , height)
+# #print(int(width), " " , int(height))
+# #img = img.resize(( width, height), Image.NEAREST)
+#
+# img = Image.open("rwot.png")
+#
+# width = int(img.size[0])
+# height = int(img.size[1])
+#
+# print(width, " " , height)
+# print(int(width), " " , int(height))
+# img = img.resize(( width, height), Image.NEAREST)
+#
+# img = img.resize(( width * 3 , height * 3), Image.NEAREST)
+#
+# img.save("intermediary.png")
+#
+#
+# tl = (1710 * 3, 2 * 3)
+#
+# final_img = Image.new('RGBA', (6000, 3000))
+# unmasked_img = Image.new('RGBA', (6000, 3000))
+# unmasked_img.paste(img, tl)
+# final_img = Image.composite(final_img, unmasked_img, mask)
+# final_img.save("generated_template.png")
