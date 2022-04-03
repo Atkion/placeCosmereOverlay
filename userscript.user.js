9edbcdf9eecfb79c32909dbd7c314f7ccd8e5b7c
atkion <giddavidr@gmail.com>
2022-04-03 13:53:44 -0600

    adding Malazan changes

--- userscript.user.js
+++ userscript.user.js
@@ -30,6 +30,14 @@ if (window.top !== window.self) {
 
                     return i;
 				})())
+		 document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://thegooddude22.github.io/bin/FantasyAlliance.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
 
 
     }, false);
