75f97fe617ceb0f70bbea7d3a60dc9bc58fb0f9f
atkion <giddavidr@gmail.com>
2022-04-03 16:30:39 -0600

    adding OnePiece template

--- userscript.user.js
+++ userscript.user.js
@@ -33,7 +33,7 @@ if (window.top !== window.self) {
 		 document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://thegooddude22.github.io/bin/FantasyAlliance.png";
+            i.src = "https://github.com/Lopeh/onepiece-place/raw/main/onepiece_template.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
