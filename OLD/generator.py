7d6ddee725e48cc090ee777c3bbf9f3b365ce8b2
atkion <giddavidr@gmail.com>
2022-04-03 21:50:17 -0600

    huh

---
+++ OLD/generator.py
@@ -0,0 +1,61 @@
+from io import BytesIO
+from PIL import Image, ImageOps
+
+from os import listdir
+from os.path import isfile, join, splitext
+
+
+PIXELART_FOLDER = "./generator/pixelart/"
+MASK_FILE = "./generator/mask.png"
+
+FIELD_WIDTH = 6000
+FIELD_HEIGHT = 3000
+
+def parse_coordinates(filename):
+    name = splitext(filename)[0]
+    return name.split("x")
+
+def normalize_coordinates(coordinates):
+    return (int(coordinates[0]) * 3, int(coordinates[1]) * 3)
+
+def process_image(filename):
+    image = Image.open(f"{PIXELART_FOLDER}/{filename}")
+    width = image.size[0]
+    height = image.size[1]
+    image = image.resize((width * 3, height * 3), Image.Dither.NONE)
+    return image
+
+def get_pixelart():
+    pixelart = [f for f in listdir(PIXELART_FOLDER) if isfile(join(PIXELART_FOLDER, f))]
+    return pixelart
+
+def open_mask():
+    mask_i = Image.open(MASK_FILE)
+    mask = Image.new("1", (FIELD_WIDTH, FIELD_HEIGHT), 0)
+    mask.paste(mask_i)
+    return mask
+
+def create_blank_image():
+    return Image.new('RGBA', (FIELD_WIDTH, FIELD_HEIGHT))
+
+def generate_template():
+    canvas = create_blank_image()
+
+    pixelart = get_pixelart()
+    print(f"Found {pixelart=}")
+
+    for filename in pixelart:
+        print(f"Processing {filename}")
+
+        image = process_image(filename)
+
+        coordinates = parse_coordinates(filename)
+        print(f"{coordinates=}")
+        canvas.paste(image, normalize_coordinates(coordinates))
+
+    mask = open_mask()
+    template = Image.composite(create_blank_image(), canvas, mask)
+    template.save("template.png")
+
+
+generate_template()
