e3cb89877066ba1f7dc4c4c172f6d4858a1ca8c5
atkion <giddavidr@gmail.com>
2022-04-03 20:41:05 -0600

    giving OP grid priority, theirs is more updated

--- userscript.user.js
+++ userscript.user.js
@@ -38,14 +38,14 @@ if (window.top !== window.self) {
             console.log(i);
             return i;
         })())
-		 document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+		/* document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
             i.src = "https://github.com/Atkion/placeCosmereOverlay/raw/master/FantasyAlliance.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
-        })())
+        })())*/
 
  
     }, false);
